9df63d999ed5b598e8a6a32acbc8d6fe75322866
WillTheVideoMan <williamhall1324@gmail.com>
2019-04-04 10:48:57 +0100

    Added env. Added more api endpoints.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
 const app = require("./app");
-const port = 8080;
+
+const port = process.env.PORT || 3003;
 app.listen(port, () => console.log("listening on port " + port));

8ce7cb4dfa31fe0dd743322e05a2a090bdf714ca
WillTheVideoMan <williamhall1324@gmail.com>
2019-06-17 12:05:53 +0100

    Commented VNC Viewer.

--- src/components/VNCViewer.js
+++ src/components/VNCViewer.js
@@ -8,50 +8,88 @@ import RFB from "@novnc/novnc";
 /**
  * VNCViewer.js - Returns a canvas with a live mirror of a defined system's desktop using VNC.
  *
- * @param {string} wsURL -
- * @param {bool} isError -
- * @param {func} onError -
- * @param {func} onReady -
+ * @param {string} wsURL - The URL of the websocket server instance.
+ * @param {bool} isError - Tells the VNC Viewer if it is in an error state, begins auto-reconnect attempts.
+ * @param {func} onError - Invoked when an error occurs.
+ * @param {func} onReady - Invoked when the VNC Viewer is ready.
  */
 const VNCViewer = ({ wsURL, isError, onError, onReady }) => {
-  const [reconnectAttempts, setReconnectAttempts] = useState(0);
-
+  /**
+   * Holds a mutable reference to the canvas' HTML DOM node.
+   */
   const screenRef = useRef();
 
-  const handleConnected = () => onReady();
+  /**
+   * Holds a stateful reference to the number of attempts made to reconnect to the VNC Server
+   * whilst in an error state.
+   */
+  const [reconnectAttempts, setReconnectAttempts] = useState(0);
 
+  /**
+   * Passes up an error object to the parent components by invoking onError from props.
+   *
+   * @param {string} message - The verbose representation of the error.
+   */
   const handleError = message => onError({ message: message });
 
+  /**
+   * Handles any disconnection events from noVNC RFB object instance.
+   *
+   * @param {obj} e - The event object passed from noVNC.
+   */
   const handleDisconnected = e => {
+    /**
+     * If the disconnection was not clean, then some error occurred. Pass an appropriate error message.
+     * Else, it was a clean connection closure. It's not technically an 'error, but nevertheless there is
+     * no longer a connection to the VNC instance, so an appropriate error message is passed.
+     */
     if (!e.detail.clean) handleError("Could not connect to VNC server.");
     else handleError("Disconnected from VNC server.");
   };
 
+  /**
+   * Informs the parent that the VNC server requires credentials to connect.
+   * Currently the viewer only allows password-less VNC servers.
+   */
   const handleCredsRequired = () =>
     handleError("VNC server requires credentials.");
 
+  /**
+   * Informs the parent that some security error occurred during connection setup.
+   */
   const handleSecurityFailure = () =>
     handleError("Security failure during connection to VNC server.");
 
+  /**
+   * If in an error state, attempt to reconnect every 30 seconds.
+   * Reconnection attempts are initiated every time the value of reconnectAttempts changes.
+   */
   useInterval(
     () => setReconnectAttempts(reconnectAttempts + 1),
     isError ? 30000 : null
   );
 
+  /**
+   * Setup a new Realtime Frame Buffer (RFB) instance, binding event listeners.
+   *
+   * Returns a cleanup function which removes all event listeners.
+   *
+   * The effect hook is invoked any time the `wsURL` or `reconnectAttempts` changes.
+   */
   useEffect(() => {
     let rfb = new RFB(screenRef.current, wsURL);
     rfb.background = "rgba(0, 0, 0, 0)";
     rfb.scaleViewport = true;
     rfb.clipViewport = true;
     rfb.viewOnly = true;
-    rfb.addEventListener("connect", handleConnected);
+    rfb.addEventListener("connect", onReady);
     rfb.addEventListener("disconnect", handleDisconnected);
     rfb.addEventListener("credentialsrequired", handleCredsRequired);
     rfb.addEventListener("securityfailure", handleSecurityFailure);
 
     return () => {
       rfb.disconnect();
-      rfb.removeEventListener("connect", handleConnected);
+      rfb.removeEventListener("connect", onReady);
       rfb.removeEventListener("disconnect", handleDisconnected);
       rfb.removeEventListener("credentialsrequired", handleCredsRequired);
       rfb.removeEventListener("securityfailure", handleSecurityFailure);
@@ -59,6 +97,9 @@ const VNCViewer = ({ wsURL, isError, onError, onReady }) => {
     };
   }, [wsURL, reconnectAttempts]);
 
+  /**
+   * Return the VNC screen.
+   */
   return <Screen ref={screenRef} />;
 };
 
@@ -71,6 +112,13 @@ VNCViewer.propTypes = {
 
 export default VNCViewer;
 
+/**
+ * Define a styled screen.
+ *
+ * noVNC applies annoying sizing rules, so a lot of the following styled attempt to override this.
+ *
+ * TODO: Try to improve dynamic sizing of noVNC canvas element.
+ */
 const Screen = styled.div`
   height: 100%;
   width: calc(100% - 1.6rem);
